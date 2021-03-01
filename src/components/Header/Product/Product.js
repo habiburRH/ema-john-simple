db96024db95f7162e9c21275514ac4ff59229439
habiburRH <habib.fafoor@gmail.com>
2021-03-01 14:49:41 +0600

    shop page codding done

---
+++ src/components/Header/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css'
+
+const product = (props) => {
+    const {img,name,seller,price,stock}=props.product;
+    
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+            <h4 className="product-name">{name}</h4>
+            <br/>
+            <p><small>by:{seller}</small></p>
+            <p>${price}</p>
+            <p><small>Only {stock} left in stock -Order soon</small></p>
+            <button className="main-button"
+            onClick={() =>props.handleAddProduct(props.product)}
+            >add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default product;
